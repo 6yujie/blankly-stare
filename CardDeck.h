3d477f73f4f24c8396f4a33ee32563fb1fd954a8
yuliu <liuyujie1994@126.com>
2018-11-26 10:39:45 +0800

    添加了一些函数功能注释

--- CardDeck.h
+++ CardDeck.h
@@ -8,9 +8,13 @@ class CardDeck
 {
 public:
     CardDeck(int decks = 1);
+    //判断是否是空牌堆
     bool empty_deck();
+    //从别的牌堆得到一张牌
     void get_one_card(Card cd);
+    //给别的牌堆一张选定的牌
     void give_selected_card(CardDeck &to, Card cd);
+    //把自己的第一张牌给别的牌堆
     void give_one_card(CardDeck &to);
 
     void print_cards();

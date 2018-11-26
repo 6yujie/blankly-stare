941716d078827cd696515f670db5717e3768b9dc
yuliu <liuyujie1994@126.com>
2018-11-26 16:11:44 +0800

    增加和修改了一些函数

--- CardDeck.cpp
+++ CardDeck.cpp
@@ -7,6 +7,7 @@ CardDeck::CardDeck(int decks)
         add_one_deck();
     }
 
+    srand(QTime(0, 0, 0).secsTo(QTime::currentTime()));
     random_shuffle(deck.begin(), deck.end());
 }
 
@@ -14,6 +15,54 @@ CardDeck::CardDeck(int decks)
 bool CardDeck::empty_deck()
 {
     return deck.empty();
+}
+
+//返回牌堆中牌的数量
+int CardDeck::size()
+{
+    return (int)deck.size();
+}
+
+//确认牌堆里有n张某点数的牌，不考虑花色
+bool CardDeck::find(Card cd, int n)
+{
+    if(n > 0)
+    {
+        for(unsigned int i = 0; i < deck.size(); i++)
+        {
+            //依次比较牌堆中各牌的点数，每次找到一个一样的就将n减一。
+            if(cd.get_point() == deck[i].get_point())
+            {
+                n--;
+            }
+
+            //n减为0时说明牌堆中有n张牌，可以跳出for循环去进行判断
+            if(n == 0)
+            {
+                break;
+            }
+        }
+
+        //判断是否找到了n张牌cd
+        if(n == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    //牌堆中有0张牌cd永远是对的
+    else if(n == 0)
+    {
+        return true;
+    }
+    //输入范围错误时直接返回false
+    else
+    {
+        return false;
+    }
 }
 
 //从别的牌堆得到一张牌
@@ -25,7 +74,7 @@ void CardDeck::get_one_card(Card cd)
 //给一张选定的牌到别的牌堆
 void CardDeck::give_selected_card(CardDeck &to, Card cd)
 {
-    vector<Card>::iterator it = find(deck.begin(), deck.end(), cd);
+    vector<Card>::iterator it = std::find(deck.begin(), deck.end(), cd);
     if(it != deck.end())
     {
         to.get_one_card(cd);
@@ -54,12 +103,43 @@ void CardDeck::add_one_deck()
 
     for(int i = ace; i <= king;  i++)
     {
-        for(int j = heart; j <= diamond; j++)
+        for(int j = spade; j <= diamond; j++)
         {
             Card tmp_card(Card::card_point_cast(i), Card::card_color_cast(j));
             deck.push_back(tmp_card);
         }
     }
+}
+
+//判断是否包含子牌堆sub
+bool CardDeck::contains(vector<Card> sub)
+{
+    if(sub.size() > deck.size())
+    {
+        return false;
+    }
+
+    //将sub先排序，可以将相同点数的牌集中
+    sort(sub.begin(), sub.end());
+
+    for(unsigned int i = 0; i < sub.size(); i++)
+    {
+        int count = 1;
+        unsigned int j = i;
+
+        while(i < sub.size() - 1 && sub[i].get_point() == sub[i+1].get_point())
+        {
+            count++;
+            i++;
+        }
+
+        if(find(sub[j], count) != true)
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 void CardDeck::print_cards()
--- CardDeck.h
+++ CardDeck.h
@@ -8,14 +8,32 @@ class CardDeck
 {
 public:
     CardDeck(int decks = 1);
+
     //判断是否是空牌堆
     bool empty_deck();
+
+    //返回牌堆中牌的数量
+    int size();
+
+    //确认牌堆里有n张某点数的牌，不考虑花色
+    bool find(Card cd, int n = 1);
+
+    vector<Card> data()
+    {
+        return deck;
+    }
+
     //从别的牌堆得到一张牌
     void get_one_card(Card cd);
+
     //给别的牌堆一张选定的牌
     void give_selected_card(CardDeck &to, Card cd);
+
     //把自己的第一张牌给别的牌堆
     void give_one_card(CardDeck &to);
+
+    //判断是否包含一组牌
+    bool contains(vector<Card> sub);
 
     void print_cards();
     void sort_cards();
